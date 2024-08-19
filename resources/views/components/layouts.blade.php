e13c3fda47bed09efd8053a80b83e5293921eef2
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-08-19 14:49:20 +0300

    Worked On The Logo

--- resources/views/components/layouts.blade.php
+++ resources/views/components/layouts.blade.php
@@ -1,19 +1,29 @@
-<!doctype html>
+ <!doctype html>
 <html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-    content="width=device-width, user- scalable=no, initial-scale=1.0, maximum-scale-1.0, minimum-scale=1.0">
+          content="width=device-width, user- scalable=no, initial-scale=1.0, maximum-scale-1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pixel Positions</title>
+    @vite(['resources/js/app.js'])
 </head>
 <body>
-    <div>
-        <nav></nav>
+<div>
+    <nav>
+        <div>
+            <!--Logo-->
+            <a href="/">
+                <img src="{{Vite::asset('resources/images/logo.svg')}}" alt="">
+            </a>
+        </div>
+        <div>//Navigation</div>
+        <div>//Post a Job</div>
+    </nav>
 
-        <main>
-            {{ $slot }}
-        </main>
-    </div>
+    <main>
+        {{ $slot }}
+    </main>
+</div>
 </body>
 </html>
