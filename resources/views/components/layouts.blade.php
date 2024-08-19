9aca004ad34fa1c22490727e456215f8ca35c3e1
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-08-19 13:37:26 +0300

    My Laravel New project

---
+++ resources/views/components/layouts.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+    content="width=device-width, user- scalable=no, initial-scale=1.0, maximum-scale-1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pixel Positions</title>
+</head>
+<body>
+    <div>
+        <nav></nav>
+
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
+</body>
+</html>
