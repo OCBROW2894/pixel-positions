7b4dc90dc238ea75259e21f4dccb7dd016201fdf
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-08-20 18:25:22 +0300

    Added some Design To The Job Card

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,12 +12,12 @@
 
                 <div class="flex justify-between items-center mt-auto">
                     <div>
-                       <a href="#">Tag</a>
-                       <a href="#">Tag</a>
-                       <a href="#">Tag</a>
+                       <a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
+                       <a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
+                       <a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
                     </div>
 
-                    <img src="http://placehold.it/42/42" alt="">
+                    <img src="http://placehold.it/42/42" alt="" class="rounded-xl">
                 </div>
         </div>
     </section>
