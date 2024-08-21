ecc3a1bbe8d9ac69e55c3c0e8cc29292477c8c46
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-08-21 11:50:40 +0300

    Added The Search Section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,13 @@
 <x-layouts>
     <div class="space-y-10">
-        <section>
+        <section class="text-center pt-6">
+            <h1 class="font-bold text-4xl">Let's Find Your Next Job</h1>
+
+            <form action="" class="mt-6">
+                <input type="text" placeholder="Web Developer?" class="rounded-xl bg-white/5 border-white/10 px-5 py-4 w-full max-w-xl">
+            </form>
+        </section>
+        <section class="pt-10">
             <x-section-heading>Featured Jobs</x-section-heading>
 
             <div class="grid lg:grid-cols-3 gap-8 mt-6">
